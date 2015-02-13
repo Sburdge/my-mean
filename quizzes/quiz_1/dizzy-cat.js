fe1f0f0a154cde18f29ffc81afc418a7cf01bf5d
Sariah <smasterson@verisagellc.com>
2015-02-12 19:49:59 -0700

    1

---
+++ quizzes/quiz_1/dizzy-cat.js
@@ -0,0 +1,12 @@
+
+var array = ['d', 'i', 'z', 'z', 'y'];
+
+for (var banana=0; banana < array.length; banana++) {
+	console.log (array[banana])
+}
+
+var array2 = ['s', 'a', 'r', 'i', 'a', 'h'];
+
+for (var i=0; i<array2.length; i++) {
+	console.log  (array2[i+0])
+}
